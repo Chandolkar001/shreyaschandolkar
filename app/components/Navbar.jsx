92304d23181ad94fbf04ebcc8f4f56cfa8a56394
Chandolkar001 <chandolkarshreyas001@gmail.com>
2023-12-31 16:12:48 +0530

    basic responsiveness completed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,31 @@
-import Link from "next/link"
+"use client"
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/skills">Skills</Link>
-        <Link href="/experience">Experience</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/social">Social</Link>
+    <nav className={menuOpen ? "active" : ""}>
+      <div id="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </div>
+      <div className={`menu-links ${menuOpen ? 'open' : ''}`}>
+        <Link href="/" onClick={closeMenu}>Home</Link>
+        <Link href="/about" onClick={closeMenu}>About</Link>
+        <Link href="/skills" onClick={closeMenu}>Skills</Link>
+        <Link href="/experience" onClick={closeMenu}>Experience</Link>
+        <Link href="/projects" onClick={closeMenu}>Projects</Link>
+        <Link href="/social" onClick={closeMenu}>Social</Link>
+      </div>
     </nav>
-  )
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,59 +1,26 @@
-"use client";
+"use client"
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '50vh', 
-  };
-
-  const imageContainerStyle = {
-    borderRadius: '50%',
-    overflow: 'hidden',
-    width: '250px',
-    height: '250px',
-  };
-
-  const imageStyle = {
-    width: '100%',
-    height: '100%',
-  };
-
-  const textContainerStyle = {
-    fontSize: '2em',
-    marginTop: '20px', 
-  };
-
-  const textstyle ={
-    color: '#3498db', // Change text color
-    fontWeight: 'bold', // Add bold font weight
-  }
-
   return (
-    <main>
-      <div style={containerStyle}>
-        <div style={imageContainerStyle}>
-          <Image src="/shreyas.jpg" alt="Shreyas" width={250} height={250} style={imageStyle} />
-        </div>
-        <div style={textContainerStyle}>
-          <TypeAnimation
-            sequence={[
-              "Software Developer",
-              1000,
-              "Robotics Developer",
-              1000,
-              "DevOps",
-              1000,
-            ]}
-            speed={20}
-            repeat={Infinity}
-            style={textstyle}
-          />
-        </div>
+    <main className="flex flex-col items-center justify-center">
+      <div className="rounded-full overflow-hidden w-48 h-48 md:w-64 md:h-64">
+        <Image src="/shreyas.jpg" alt="Shreyas" width={250} height={250} layout="responsive" className="rounded-full" />
+      </div>
+      <div className="text-center mt-4 text-lg md:text-2xl font-bold text-blue-500">
+        <TypeAnimation
+          sequence={[
+            "Software Developer",
+            1000,
+            "Robotics Developer",
+            1000,
+            "DevOps",
+            1000,
+          ]}
+          speed={20}
+          repeat={Infinity}
+        />
       </div>
     </main>
   );
