626d68f373d8669494fc134a91216857791c8214
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-02 19:52:01 +0530

    minor changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,23 @@
 "use client"
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
   return (
     <main className="flex flex-col items-center justify-center">
       <div className="rounded-full overflow-hidden w-48 h-48 md:w-64 md:h-64">
-        <Image src="/shreyas.jpg" alt="Shreyas" width={250} height={250} layout="responsive" className="rounded-full" />
+        <Image
+          src="/shreyas.jpg"
+          alt="Shreyas"
+          width={250}
+          height={250}
+          className="rounded-full"
+          sizes="100vw"
+          loading="lazy"
+          style={{
+            width: "100%",
+            height: "auto"
+          }} />
       </div>
       <div className="text-center mt-4 text-lg md:text-2xl font-bold text-blue-500">
         <TypeAnimation
